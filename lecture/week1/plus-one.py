49298fc66b6f480ae63a36e6c6dc9c90fd87b607
Hailemariam Kefale <95341678+Hailemari@users.noreply.github.com>
2023-12-20 22:45:22 +0300

    Added solution for Plus One

--- lecture/week1/plus-one.py
+++ lecture/week1/plus-one.py
@@ -1,11 +1,18 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         n = len(digits)
-        for i in range(n-1, -1, -1):
-            
-            if digits[i] < 9:
+
+        # If the last digit is not 9, simply increment it
+        if digits[-1] != 9:
+            digits[-1] += 1
+            return digits
+
+        # If the last digit is 9, find the rightmost non-9 digit and increment it
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] != 9:
                 digits[i] += 1
+                digits[i+1:] = [0] * (n-i-1) # Set all digits to the right to 0
                 return digits
-            else:
-                digits[i] = 0
-        return [1] + digits
+        
+        # If all digits are 9, add a new leading digit
+        return [1] + [0] * n
