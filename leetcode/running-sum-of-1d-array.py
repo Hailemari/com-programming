6f59c9f708030968e5e1cabe4d429fb08582212a
Hailemariam Kefale <95341678+Hailemari@users.noreply.github.com>
2024-02-07 15:08:01 +0300

    Add solution for Running Sum of 1d Array

--- leetcode/running-sum-of-1d-array.py
+++ leetcode/running-sum-of-1d-array.py
@@ -1,10 +1,9 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        prefix_sum = [nums[0]] * n
+        accumulator = 0
+        for i in range(n):
+            nums[i] += accumulator
+            accumulator = nums[i]
 
-        for i in range(1, n):
-            prefix_sum[i] = prefix_sum[i-1] + nums[i]
-
-        return prefix_sum
-            
+        return nums
