f60f7fd7716664b87d0c6ba887b2e1a4b7af4def
Hailemariam Kefale <95341678+Hailemari@users.noreply.github.com>
2024-02-09 11:34:40 +0300

    Add solution for Subarray Sums Divisible by K

--- leetcode/subarray-sums-divisible-by-k.py
+++ leetcode/subarray-sums-divisible-by-k.py
@@ -1,11 +1,17 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        count = {0: 1}  # Initialize the count of remainder 0 to 1
-        prefix_sum = 0
-        result = 0
-        for num in nums:
-            prefix_sum = (prefix_sum + num) % k
-            if prefix_sum in count:
-                result += count[prefix_sum]
-            count[prefix_sum] = count.get(prefix_sum, 0) + 1
-        return result
+        pref_sum = 0
+        count_array = { }
+        answer = 0
+
+        for pref_sum in accumulate(nums):
+            pref_sum %= k
+            if pref_sum == 0:
+                answer += 1
+
+            if pref_sum in count_array:
+                answer += count_array[pref_sum]
+
+            count_array[pref_sum] = count_array.get(pref_sum, 0) + 1
+
+        return answer
