dc4aa9b50c293a4e08c08f375d68d99a232f1e74
Hailemariam Kefale <95341678+Hailemari@users.noreply.github.com>
2024-02-09 16:53:43 +0300

    Add solution for Minimum Penalty for a Shop

--- leetcode/minimum-penalty-for-a-shop.py
+++ leetcode/minimum-penalty-for-a-shop.py
@@ -1,31 +1,21 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
         n = len(customers)
+        num_Ys = customers.count("Y")
+        num_Ns = n - num_Ys
+        answer = [0] * n
         
-        total_Y = customers.count("Y")
-        total_N = 0
-        num_y_arr = [total_Y]
-        num_n_arr = [total_N]
-
         for i in range(n):
+            answer[i] = num_Ys
             if customers[i] == "Y":
-                total_Y -= 1
-                num_y_arr.append(total_Y)
-            else:
-                num_y_arr.append(total_Y)
-            
-        for i in range(n):
+                num_Ys -= 1
+        
+        answer.append(0)
+        answer[-1] += num_Ns
+    
+        for i in range(n - 1, -1, -1):
             if customers[i] == "N":
-                num_n_arr.append(total_N)
-                total_N += 1
-            else:
-                num_n_arr.append(total_N)
-        num_y_arr.append(0)
-        num_n_arr.append(total_N)
-        ans = []
-        for left, right in zip(num_y_arr, num_n_arr):
-            ans.append(left + right)
-        
-        return ans.index(min(ans))
-
-        
+                num_Ns -= 1
+            answer[i] += num_Ns
+            
+        return answer.index(min(answer))
