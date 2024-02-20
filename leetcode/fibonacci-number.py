d0e5d77ef6838698b282d3f0e12a98bb195c6308
Hailemariam Kefale <95341678+Hailemari@users.noreply.github.com>
2024-02-20 18:39:21 +0300

    Add solution for Fibonacci Number

--- leetcode/fibonacci-number.py
+++ leetcode/fibonacci-number.py
@@ -1,8 +1,11 @@
+memo = {}
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
-
+        if n <= 1:
+            return n
+        
+        if n in memo:
+            return memo[n]
+        
+        memo[n] = self.fib(n-1) + self.fib(n-2)
         return self.fib(n-1) + self.fib(n-2)
