fe50ce10f910ad477b8ddf5cbba05d83a8ee4729
Hailemariam Kefale <95341678+Hailemari@users.noreply.github.com>
2024-02-29 14:22:44 +0300

    Add solution for Combination Sum

--- leetcode/combination-sum.py
+++ leetcode/combination-sum.py
@@ -2,19 +2,21 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
 
-        def backtrack(i, path):
-            if sum(path) == target:
+        def backtrack(i, path, path_sum):
+            if path_sum == target:
                 if path[:] not in ans:
                     ans.append(path[:])
                 return
                 
-            if sum(path) > target:
+            if path_sum > target:
                 return
 
             for j in range(i, len(candidates)):
+                path_sum += candidates[j]
                 path.append(candidates[j])
-                backtrack(j, path)
+                backtrack(j, path, path_sum)
+                path_sum -= candidates[j]
                 path.pop()
             
-        backtrack(0, [])
+        backtrack(0, [], 0)
         return ans
