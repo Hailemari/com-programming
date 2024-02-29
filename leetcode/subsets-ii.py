6cddcc905b5763e90e7224c25a1f36ab7ddab557
Hailemariam Kefale <95341678+Hailemari@users.noreply.github.com>
2024-02-29 15:22:13 +0300

    Add solution for Subsets II

--- leetcode/subsets-ii.py
+++ leetcode/subsets-ii.py
@@ -3,18 +3,17 @@ class Solution:
         n = len(nums)
         ans = []
         def backtrack(i, subset):
-            if i == n:
+            if i == n + 1:
                 return
+            cur = sorted(subset)               
+            if cur not in ans:
+                ans.append(cur[:])
                 
             for j in range(i, n):
                 subset.append(nums[j])
-                backtrack(j+1, subset)
-                cur = sorted(subset)
-                
-                if cur not in ans:
-                    ans.append(cur[:])
+                backtrack(j+1, subset)                
                 subset.pop()
 
         backtrack(0, [])
 
-        return [[]] + ans
+        return ans
