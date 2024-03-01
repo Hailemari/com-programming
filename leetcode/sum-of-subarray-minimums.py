b90d0e187bc7a92eda268569b7b5e9cb349421a9
Hailemariam Kefale <95341678+Hailemari@users.noreply.github.com>
2024-03-01 23:02:06 +0300

    Add solution for Sum of Subarray Minimums

--- leetcode/sum-of-subarray-minimums.py
+++ leetcode/sum-of-subarray-minimums.py
@@ -1,35 +1,18 @@
 class Solution:
     def sumSubarrayMins(self, arr: List[int]) -> int:
-        n = len(arr)
         MOD = 10**9 + 7
-        prev_smaller = [-1]*n
-        next_smaller = [-1]*n
-
-        stack2 = []
-        for i, num in enumerate(arr):
-            if stack2:
-                while stack2 and arr[stack2[-1]] >= num:
-                    next_smaller[stack2[-1]] = i
-                    stack2.pop()
-            stack2.append(i)
-
+        result = 0
         stack = []
-        for i in range(n - 1, -1, -1):
-            if stack:
-                while stack and arr[stack[-1]] > arr[i]:
-                    prev_smaller[stack[-1]] = i
-                    stack.pop()
-            stack.append(i)
-        
-        ans = 0
-        for i in range(n):
-            if next_smaller[i] == -1:
-                ans += ((i - prev_smaller[i])*(n - i))*arr[i]
-            else:
-                ans += ((i - prev_smaller[i])*(next_smaller[i] - i))*arr[i]
+        arr.append(0)
 
-        
-        return ans % (10**9 + 7)
-            
-            
-            
+        for i, num in enumerate(arr):
+            while stack and arr[stack[-1]] > num:
+                idx = stack.pop()
+                left = idx - stack[-1] if stack else idx + 1
+                right = i - idx
+                result += arr[idx] * left * right
+                result %= MOD
+
+            stack.append(i)
+
+        return result
