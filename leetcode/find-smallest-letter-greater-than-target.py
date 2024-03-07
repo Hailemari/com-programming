15e0fa178f4b4675730723cb0ba56234c2d16320
Hailemariam Kefale <95341678+Hailemari@users.noreply.github.com>
2024-03-07 11:30:27 +0300

    Add solution for Find Smallest Letter Greater Than Target

---
+++ leetcode/find-smallest-letter-greater-than-target.py
@@ -0,0 +1,18 @@
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        left = 0
+        right = len(letters) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if ord(letters[mid]) > ord(target):
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        if right == len(letters) - 1:
+            return letters[0]
+            
+        else:
+            return letters[left]
